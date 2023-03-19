c5a4af2fce21f3141e9262f0259944cf936e0b16
Misaki-ohime <113137090+Misaki-ohime@users.noreply.github.com>
2023-03-19 19:14:34 -0300

    Added BASE project
    Unstable. Under construction.

---
+++ antibiotic_list.js
@@ -0,0 +1,115 @@
+import Form from 'react-bootstrap/Form';
+
+const quinolones =[
+"Norfloxacin",
+"Orfloxacin",
+"Nalixid Acid",
+"Perfloxacin",
+"Enofloxacin",
+"Levofloxacin",
+"Ciprofloxacin"
+];
+
+const sulfonamide =[
+"Trimethopim",
+"Trimethoprim/Sulfamethoxazole",
+"Sulfamethoxazole",
+"Sulfonamide",
+"Sulfafurazol",
+"Sulfazotrim"
+];
+
+const Penicilin = [
+	"Carbenicilin",
+	"Ticarciclin",
+	"Amoxicilin",
+	"Amoxicilin-clavulanato",
+	"Piperacillin",
+	"Piperacillin-tazobactam",
+	"Ampicilin-sulbactam",
+	"Ampicilin"
+	];
+
+const Monobactam = [
+	"Aztreonam"
+	];
+
+const Cephalosporin = [
+	"Ceftiofur",
+	"Cefazolin",
+	"Cefoperazone",
+	"Cefoxitin",
+	"Cefaclor",
+	"Cephalotin",
+	"Cefepime",
+	"Cefotaxime",
+	"Ceftazidime",
+	"Cefexime",
+	"Cefuroxime",
+	"Ceftriaxone"
+	];
+
+const Aminoglycoside = [
+	"Neomycin",
+	"Tobramycin",
+	"Kanamycin",
+	"Amikacin",
+	"Streptomycin",
+	"Gentamicin"
+	];
+
+const Carbapenem = [
+	"Ertapenem",
+	"Meropenem",
+	"Imipenem"
+	];
+
+const Macrolides = [
+	"Erythromicin",
+	"Azithromycin"
+	];
+
+const Phenicols = [
+	"Florfenicol",
+	"Cloramphenicol"
+	];
+
+const Polymyxins = [
+	"Polymyxin-E",
+	"Polymyxin-B"
+	];
+
+const Phospomycins = [
+	"Fosfomycin"
+	];
+
+const Tetracycline = [
+"Doxycicline",
+"Oxitetracicline",
+"Minocycline",
+"Tetracycline"
+	];
+
+const tetraclass = {Name: Tetracycline, Abbr:["DOX", "OXI","MIN","TET"]}
+
+import Form from 'react-bootstrap/Form';
+
+function SwitchAntibiotic() {
+  return (
+    <Form>
+      <Form.Check 
+        type="switch"
+        id="custom-switch"
+        label="Check this switch"
+      />
+      <Form.Check 
+        disabled
+        type="switch"
+        label="disabled switch"
+        id="disabled-custom-switch"
+      />
+    </Form>
+  );
+}
+
+export default SwitchExample;
